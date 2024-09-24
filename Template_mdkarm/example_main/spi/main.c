d5d944127a8b902a42a341940a85d5340092b291
TuanAnh <Anhconstant@gmail.com>
2024-09-24 17:07:48 +0700

    update
    update spi main example

--- Template_mdkarm/example_main/spi/main.c
+++ Template_mdkarm/example_main/spi/main.c
@@ -3,8 +3,10 @@ int main(void)
 {	
 	SPI_config();
 	GPIO_config()	;
+	uint16_t data[3] = {'A','B','C'};
 	while (1) {
-		SPI_I2S_SendData(SPI1,'A');
+		
+		SPI_send_byte(SPI1,data,3);
 		GPIO_SetBits(GPIOB, GPIO_Pin_2|GPIO_Pin_1);
 		delaynop();
 		GPIO_ResetBits(GPIOB, GPIO_Pin_2|GPIO_Pin_1);
